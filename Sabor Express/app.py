16647e34fe2458e40827442e8236bf51feea9b14
Renzo <renzomesquita25@gmail.com>
2026-06-10 22:53:30 -0300

    Starting new project

---
+++ Sabor Express/app.py
@@ -0,0 +1,10 @@
+print('''
+█▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
+▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█ ▄█''')
+
+print('1. Cadastrar')
+print('2. Listar Restaurante')
+print('3. Ativar Restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = input('Escolha uma opção: ')
