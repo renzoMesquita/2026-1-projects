e9cf58bab1a1bd9ec54616585f37aac0048da29f
Renzo <renzomesquita25@gmail.com>
2026-06-11 13:17:57 -0300

    New elemets to the python app

--- Sabor Express/app.py
+++ Sabor Express/app.py
@@ -1,10 +1,36 @@
-print('''
+import os
+
+def exibir_nome_do_programa():
+    print('''
 █▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
 ▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█ ▄█''')
 
-print('1. Cadastrar')
-print('2. Listar Restaurante')
-print('3. Ativar Restaurante')
-print('4. Sair\n')
+def exibir_opcoes():
+    print('2. Listar Restaurante')
+    print('3. Ativar Restaurante')
+    print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
+def encerrar():
+    os.system('clear')
+    print('Encerrando o programa')
+
+def escolher_opcoes():
+    opcao_escolhida = input('Escolha uma opção: ')
+
+    if opcao_escolhida == '1':
+        print('Cadastrar Restaurante')
+    elif opcao_escolhida == '2':
+        print('Listar Restaurantes')
+    elif opcao_escolhida == '3':
+        print('Ativar Restaurante')
+    else: 
+        encerrar()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcoes()
+
+if __name__ == '__main__':
+    main()
+    
