fb949a2593957d6fd04a50a786aebfcaf9029dae
Renzo <renzomesquita25@gmail.com>
2026-06-14 19:37:41 -0300

    Listagem de Restaurantes atualizada

--- Sabor Express/app.py
+++ Sabor Express/app.py
@@ -33,7 +33,9 @@ def opcao_invalida():
 def cadastrar_novo_restaurante():
     exibir_subtitulo('Cadastro')
     nome_do_restaurante = input('Digite o nome do restaurante: ')
-    restaurantes.append(nome_do_restaurante)
+    categoria_restaurante = input(f'Digite a categoria do {nome_do_restaurante}: ')
+    dados_do_restaurante = {'nome' : nome_do_restaurante, 'categoria' : categoria_restaurante, 'ativo' : False}
+    restaurantes.append(dados_do_restaurante)
     print(f'O restaurante {nome_do_restaurante} foi cadastrado')
     voltar_ao_menu()
 
