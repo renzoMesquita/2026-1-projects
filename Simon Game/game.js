371cfe1de9a7b997959f4642c231160caa348ccb
Renzo <renzomesquita25@gmail.com>
2026-06-23 21:05:58 -0300

    Click function - Keyboard and mouse

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -2,6 +2,8 @@ let gamePattern = []
 let userClickedPattern = []
 let randomNumber
 
+let level = 0;
+
 const buttonColor = ["red", "blue", "green", "yellow"]
 
 function nextSequence(){
@@ -12,17 +14,40 @@ function nextSequence(){
     let audio = new Audio("sounds/" + randomChosenColor + ".mp3")
     audio.play()
 
+    level++
+
+    $("h1").html("Level " + level)
+
     $("#" + randomChosenColor).addClass("pressed")
     setTimeout(function(){
         $("#" + randomChosenColor).removeClass("pressed")
     }, 100)
 }
 
+let started = false
+
+$(document).keypress(function(){
+    $("h1").html("Level " + level)
+    $("h1").addClass("level-title")
+    if(!started){
+        nextSequence()
+        started = true
+    }
+})
+
 $(".btn").click(function() {
     let userChosenColor = $(this).attr("id")
     userClickedPattern.push(userChosenColor)
+
+    $("#" + userChosenColor).addClass("pressed")
+    setTimeout(function(){
+        $("#" + userChosenColor).removeClass("pressed")
+    }, 100)
+
+    let audio = new Audio("sounds/" + userChosenColor + ".mp3")
+    audio.play()
+
     console.log(userClickedPattern)
-    nextSequence()
 })
 
 
